0a03fe3e0c15fdb97b230586ba45f6fbee683a2d
Shinya Hayashi <shinyaUT0925@gmail.com>
2023-03-31 13:07:55 +0900

    change the structure of input yaml
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -47,15 +47,6 @@ func TestBasicScenario(t *testing.T) {
 	assert.True(t, strings.Contains(metrics, `test1{aaa="aaa_val1",bbb="bbb_val2"} 1`))
 	assert.True(t, strings.Contains(metrics, `test2{aaa="aaa_val2",ccc="ccc_val1"} 1`))
 
-	// post additional recipe
-	f, err = os.Open("additional-recipe.yaml")
-	require.NoError(t, err)
-
-	resp, err = http.Post(baseURL+"/recipe", "application/yaml", f)
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	f.Close()
-
 	// get metrics 3 (the value of test2 will be drained)
 	resp, err = http.Get(baseURL + "/metrics")
 	require.NoError(t, err)
@@ -68,7 +59,6 @@ func TestBasicScenario(t *testing.T) {
 	assert.True(t, strings.Contains(metrics, `test1{aaa="aaa_val1",bbb="bbb_val1"} 6`))
 	assert.True(t, strings.Contains(metrics, `test1{aaa="aaa_val1",bbb="bbb_val2"} 2`))
 	assert.True(t, strings.Contains(metrics, `test2{aaa="aaa_val2",ccc="ccc_val1"} 0`))
-	assert.True(t, strings.Contains(metrics, `test2{aaa="aaa_val2",ccc="ccc_val2"} 2`))
 
 	// get metrics 4 (the value of test1{aaa="aaa_val1", bbb="bbb_val1"} will be drained)
 	resp, err = http.Get(baseURL + "/metrics")
@@ -82,7 +72,6 @@ func TestBasicScenario(t *testing.T) {
 	assert.True(t, strings.Contains(metrics, `test1{aaa="aaa_val1",bbb="bbb_val1"} 6`))
 	assert.True(t, strings.Contains(metrics, `test1{aaa="aaa_val1",bbb="bbb_val2"} 3`))
 	assert.True(t, strings.Contains(metrics, `test2{aaa="aaa_val2",ccc="ccc_val1"} 0`))
-	assert.True(t, strings.Contains(metrics, `test2{aaa="aaa_val2",ccc="ccc_val2"} 2`))
 
 	// delete recipe
 	req, err := http.NewRequest(http.MethodDelete, baseURL+"/recipe", nil)
